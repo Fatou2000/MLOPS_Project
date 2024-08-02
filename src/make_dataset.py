5582c11b8bcab2bd56a7cdfc31cc070aad827240
bintou10 <ngbtening@ept.sn>
2024-08-02 22:34:30 +0000

    Test02_08-V2

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -2,6 +2,10 @@ import os
 import numpy as np
 import cv2
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
+
 def load_data(path: str) -> tuple:
     images = []
     labels = []
@@ -25,3 +29,51 @@ def load_data(path: str) -> tuple:
     images = np.array(images)
     labels = np.array(labels)
     return images, labels, class_names
+
+
+def visualize_class_distribution(labels: np.ndarray, class_names: list):
+    class_counts = Counter(labels)
+    plt.figure(figsize=(10, 6))
+    plt.bar(class_counts.keys(), class_counts.values(), tick_label=class_names)
+    plt.xlabel('Classes')
+    plt.ylabel('Counts')
+    plt.title('Distribution des classes')
+    plt.show()
+
+
+def show_random_images(images: np.ndarray, labels: np.ndarray, num_samples: int, mode: str = 'BGR2RGB'):
+    if num_samples > len(images):
+        raise ValueError("Number of samples requested exceeds available samples.")
+
+    # Randomly select indices
+    random_indices = random.sample(range(len(images)), num_samples)
+
+    class_names={j:i for i,j in labels_.items()}
+
+    selected_images = images[random_indices]
+    selected_labels = [class_names[labels[index]].capitalize() for index in random_indices]
+
+    # Define the number of rows and columns for displaying images
+    num_cols = 3
+    num_rows = (num_samples + num_cols - 1) // num_cols
+
+    plt.figure(figsize=(15, 5 * num_rows))
+
+    for i, (img, label) in enumerate(zip(selected_images, selected_labels)):
+        plt.subplot(num_rows, num_cols, i + 1)
+        if isinstance(mode,str) and not mode is None:
+            mode=mode.upper()
+        if mode == 'GRAY':
+            img_display = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            plt.imshow(img_display, cmap='gray')
+        elif mode == 'BGR2RGB':
+            img_display = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            plt.imshow(img_display)
+        elif mode == 'RGB':
+            plt.imshow(img)
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+        plt.title(label)
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
