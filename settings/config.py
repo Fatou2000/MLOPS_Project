054b6616a027588a623a2b7df13b03459e864675
bintou10 <ngbtening@ept.sn>
2024-08-05 17:00:11 +0000

    MLFLOW on Colab configs

--- settings/config.py
+++ settings/config.py
@@ -1,19 +1,21 @@
 # Configuration for VGG19
-VGG19_CONFIG = {
+VGG16_CONFIG = {
+    'input_shape': (128, 128, 3),
     'num_classes': 6,
     'learning_rate': 1e-5,
     'batch_size': 32,
-    'epochs': 5,
+    'epochs': 30,
     'pretrained': True,
     'model_path': 'vgg19_pretrained.pth'
 }
 
 # Configuration for MobileNetV2
 MOBILENETV2_CONFIG = {
+    'input_shape': (128, 128, 3),
     'num_classes': 6,
-    'learning_rate': 1e-5,
-    'batch_size': 32,
-    'epochs': 20,
+    'learning_rate': 0.0001,
+    'epochs':50,
+    'patience':5,
     'pretrained': True,
     'model_path': 'mobilenetv2_pretrained.pth'
 }
